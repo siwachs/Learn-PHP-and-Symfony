ab463c346bb8dd2ad68825dedf479f6941dc3619
siwachs <ssiwach.8888@gmail.com>
2023-10-27 12:43:01 +0530

    Refactored

--- Assignment_1/includes/db.php
+++ Assignment_1/includes/db.php
@@ -1,7 +1,7 @@
 <?php
 $db['db_host'] = "localhost";
 $db['db_user'] = "root";
-$db['db_password'] = "";
+$db['db_password'] = "password";
 $db['db_database'] = "students";
 
 $constants = get_defined_constants();
--- Assignment_2/config.php
+++ Assignment_2/config.php
@@ -1,5 +1,5 @@
 <?php
 define('DB_HOST', 'localhost');
 define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
+define('DB_PASSWORD', 'password');
 define('DB_DATABASE', 'store');
